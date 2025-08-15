9263cb5680fb7e2a088deca0aa836e4c948efd14
alexa-gui <zg319@cam.ac.uk>
2025-08-15 11:12:08 +0800

    Initial Commit

---
+++ HumanoidRobot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HumanoidRobot
+//
+//  Created by G axa on 8/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HumanoidRobot/HumanoidRobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanoidRobotApp.swift
+//  HumanoidRobot
+//
+//  Created by G axa on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HumanoidRobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
