edb66820efb9107da9da6311ce0ea20a7d81d7b0
alexa-gui <zg319@cam.ac.uk>
2025-08-15 16:56:39 +0800

    🎯 机器人模型向上移动 - 添加负顶部间距-20px + 专门针对标题与机器人间距调整

--- HumanoidRobot/ContentView.swift
+++ HumanoidRobot/ContentView.swift
@@ -87,6 +87,7 @@ struct HomeView: View {
                             .foregroundColor(.white.opacity(0.6))
                     }
                     .padding(.horizontal, 20)
+                    .padding(.top, -20)
                     
                     // 组装按钮
                     Button(action: {
